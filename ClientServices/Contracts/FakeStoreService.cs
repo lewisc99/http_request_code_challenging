e619ac80bfb481caa5b10776172d25234b632fea
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-02-26 23:07:13 -0300

    created service FakeStoreService

---
+++ ClientServices/Contracts/FakeStoreService.cs
@@ -0,0 +1,31 @@
+﻿using CodeChallenging.ClientServices.Interfaces;
+using CodeChallenging.Entities;
+using System.Text.Json;
+
+namespace CodeChallenging.ClientServices.Contracts
+{
+    public class FakeStoreService : IFakeStoreService
+    {
+        private readonly IHttpClientFactory _clientFactory;
+
+        public FakeStoreService(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+        }
+
+        public async Task<IEnumerable<Product>> FindProductsAsync()
+        {
+            var client = _clientFactory.CreateClient("FakeStore");
+
+            var response = await client.GetAsync("products");
+
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStreamAsync();
+
+            var result = await JsonSerializer.DeserializeAsync<IEnumerable<Product>>(content);
+
+            return result;
+        }
+    }
+}
