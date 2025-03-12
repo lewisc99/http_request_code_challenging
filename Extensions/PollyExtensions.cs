073032258315f2b0b6224611f2be085837d17385
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-03-11 22:22:23 -0300

    created GetJitterRetryPolicy

--- Extensions/PollyExtensions.cs
+++ Extensions/PollyExtensions.cs
@@ -1,10 +1,12 @@
 ﻿using Polly.Extensions.Http;
 using Polly;
+using Polly.Contrib.WaitAndRetry;
 
 namespace CodeChallenging.Extensions
 {
     public static class PollyExtensions
     {
+        //old code without Jitter-Enabled
         public static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
         {
             return HttpPolicyExtensions
@@ -14,6 +16,25 @@ namespace CodeChallenging.Extensions
                 .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
                 // Retries 6 times with an exponential back-off (2^retryAttempt seconds)
                 .WaitAndRetryAsync(6, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+
+        /// <summary>
+        /// enhancing resilience against transient faults.
+        /// A regular Retry policy can affect your system in cases of high concurrency 
+        /// and scalability and under high contention.
+        /// </summary>
+        /// <returns></returns>
+        public static IAsyncPolicy<HttpResponseMessage> GetJitterRetryPolicy()
+        {
+            // Define the delay sequence with decorrelated jitter
+            var delay = Backoff.DecorrelatedJitterBackoffV2(
+                medianFirstRetryDelay: TimeSpan.FromSeconds(1),
+                retryCount: 6);
+
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(delay);
         }
     }
 }
