f6c6db20c25dcb43f5afc28239ce0611b3fef667
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-03-13 21:48:37 -0300

    Implement the Circuit Breaker pattern

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
 using CodeChallenging.ClientServices.Contracts;
 using CodeChallenging.ClientServices.Interfaces;
 using CodeChallenging.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Extensions.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,15 +15,27 @@ builder.Services.AddScoped<IFakeStoreService, FakeStoreService>();
 
 
 string fakeStoreApiUrl = builder.Configuration.GetValue<string>("API:FakeStore");
+var circuitBreakerPolicy = GetCircuitBreakerPolicy();
 
 builder.Services.AddHttpClient<IFakeStoreService, FakeStoreService>(client =>
 {
     client.BaseAddress = new Uri(fakeStoreApiUrl);
-}).SetHandlerLifetime(TimeSpan.FromMinutes(5)) 
-        // Old code without Jitter
-        //.AddPolicyHandler(PollyExtensions.GetRetryPolicy());
-        .AddPolicyHandler(PollyExtensions.GetJitterRetryPolicy());
+})
+.SetHandlerLifetime(TimeSpan.FromMinutes(5))  // Sample: default lifetime is 2 minutes
+// Old code without Jitter
+//.AddPolicyHandler(PollyExtensions.GetRetryPolicy());
+.AddPolicyHandler(PollyExtensions.GetJitterRetryPolicy())
+.AddPolicyHandler(circuitBreakerPolicy);
 
+static IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions
+        .HandleTransientHttpError()
+          .CircuitBreakerAsync(
+        5, // Number of failures before breaking the circuit
+        TimeSpan.FromSeconds(30) // Duration of break
+    );
+}
 
 string jsonPlaceholderUrl = builder.Configuration.GetValue<string>("API:JsonPlaceholder");
 
