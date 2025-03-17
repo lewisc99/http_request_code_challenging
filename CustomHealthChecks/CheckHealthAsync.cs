2ea7356cf23db01f8589db763f81730918622a2d
lewisc99 <80711658+lewisc99@users.noreply.github.com>
2025-03-16 22:35:12 -0300

    created custom CheckHealthAsync

---
+++ CustomHealthChecks/CheckHealthAsync.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace CodeChallenging.CustomHealthChecks
+{
+    public class CustomHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            // Example logic to check if a service is alive
+            bool isHealthy = true;
+
+            if (isHealthy)
+            {
+                return Task.FromResult(HealthCheckResult.Healthy("Service is running."));
+            }
+
+            return Task.FromResult(HealthCheckResult.Unhealthy("Service is down."));
+        }
+    }
+}
